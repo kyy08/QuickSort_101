eba8f543ee319091902b649f60bdbfbffb7c2f8a
kyy08 <kyyelzz1@gmail.com>
2023-03-30 18:47:26 +0700

    Swaps the element at index x with the element at index y

--- QuickSort_101/QuickSort_101.cpp
+++ QuickSort_101/QuickSort_101.cpp
@@ -5,3 +5,29 @@ using namespace std;
 int arr[20];			 // array of intergers to hold values
 int cmp_count = 0;		// number of comparasion
 int mov_count = 0;		// number of data movement
+int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukan Panjang Element Array: ";		
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum Panjang Array adalah 20" << endl;
+	}
+
+	cout << "\n====================" << endl;
+	cout << "\n Enter Element Array" << endl;
+	cout << "\n====================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
+//Swaps the element at index x with the element at index y
+
